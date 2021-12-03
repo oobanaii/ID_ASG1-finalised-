d16867967d2deaf5465555dc04cc22934939e768
oobanaii <s10223508@connect.np.edu.sg>
2021-12-03 21:48:43 +0800

    finalised

--- java.js
+++ java.js
@@ -13,13 +13,6 @@ window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("myTopnav");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
